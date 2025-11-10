0dc0b793c1726755d747d41c054e519a64fb8b45
Moritz Baumgart <baumgartmoritz@web.de>
2025-11-10 16:02:26 +0100

    feat: add --init cli flag to initialize empty workspace directory

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+from argparse import ArgumentParser
 
 from config import load_config
 from treesearch.search import TreeSearch
 from utils.log import set_log_level, _ROOT_LOGGER
+from utils.path import mkdir
 
 logger = _ROOT_LOGGER.getChild("main")
 
@@ -11,6 +13,11 @@ def main():
     set_log_level(os.getenv("ISGSA_LOG", "INFO"))
 
     config = load_config()
+    out_dir = mkdir(config.out_dir)
+    args = get_args()
+    if args.init:
+        mkdir(out_dir / "workspace")
+        return
 
     user_req_lines: list[str] = []
     print('Enter you request, write "!start" to start:')
@@ -26,6 +33,13 @@ def main():
 
     ts = TreeSearch(user_request, config=config)
     ts.run()
+
+
+def get_args():
+    parser = ArgumentParser("AutoRecLab")
+    parser.add_argument("--init", action="store_true")
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
